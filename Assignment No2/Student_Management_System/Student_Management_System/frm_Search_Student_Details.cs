b5d9e09c0120ddde1c3f1ad6162ffee17b3eb257
VaishnaviPatil753 <114859651+VaishnaviPatil753@users.noreply.github.com>
2022-10-23 13:02:00 +0530

    Add files via upload

---
+++ Assignment No2/Student_Management_System/Student_Management_System/frm_Search_Student_Details.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Student_Management_System
+{
+    public partial class frm_Search_Student_Details : Form
+    {
+        public frm_Search_Student_Details()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Student_Management_System_DB;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if (Con.State != ConnectionState.Open)
+            { 
+                Con.Open();
+            }
+        }
+        void Con_Close()
+        {
+            if (Con.State != ConnectionState.Closed)
+            {
+                Con.Close();
+            }
+        }
+        void Clear_Controls()
+        {
+            tb_Roll_No.Clear();
+            tb_Name.Clear();
+            tb_Mobile_No.Clear();
+            dtp_DOB.Text = "1/1/2005";
+            cmb_Course.SelectedIndex = -1;
+        }
+
+        private void btn_Add_New_Student_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Student obj = new frm_Add_New_Student();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void btn_View_Student_List_Click(object sender, EventArgs e)
+        {
+            frm_View_All_Student_List obj = new frm_View_All_Student_List();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            Con_Open();
+
+            SqlCommand Cmd = new SqlCommand("Select * From Student_Details Where Roll_No = @RNo ", Con);
+
+            Cmd.Parameters.Add("RNo", SqlDbType.Int).Value = tb_Roll_No.Text;
+
+            SqlDataReader Dr = Cmd.ExecuteReader();
+
+            if (Dr.Read())
+            {
+                tb_Name.Text = Dr.GetString(Dr.GetOrdinal("Name"));
+                tb_Mobile_No.Text = (Dr["Mobile_No"].ToString());
+                cmb_Course.Text = Dr.GetString(Dr.GetOrdinal("Course"));
+                dtp_DOB.Text = (Dr["DOB"].ToString());
+            }
+            else
+            {
+                MessageBox.Show("No Record Found ","Invalid Roll No");
+                tb_Roll_No.Clear();
+            }
+            Con_Close();
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            DialogResult Res = MessageBox.Show("Are You Shoure Want To Log Out???", "LOGOUT", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (Res == DialogResult.Yes)
+            {
+                Frm_Login obj = new Frm_Login();
+                obj.Show();
+                this.Hide();
+            }
+        }
+    }
+}
